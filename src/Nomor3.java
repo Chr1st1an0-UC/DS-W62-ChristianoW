1fd56a11bfefdf8c5660897e3732ab11290e46d3
Chris <cwaliliong@student.ciputra.ac.id>
2026-03-11 22:19:58 +0800

    Perbaikan Nomor 3

--- src/Nomor3.java
+++ src/Nomor3.java
@@ -19,11 +19,11 @@ public class Nomor3 {
             int n = sc.nextInt();
             sc.nextLine();
 
-            // Constraint: 5 < line < 20
             if (n < 5 || n > 20) {
                 System.out.println("Invalid queue length (must be between 5 and 20)");
                 return;
             }
+
 
             String[] names = sc.nextLine().split(" ");
             String[] chanceInput = sc.nextLine().split(" ");
@@ -35,9 +35,8 @@ public class Nomor3 {
                 String name = names[i];
                 int chance = Integer.parseInt(chanceInput[i]);
 
-                // Constraint: chance 1–10
-                if (chance < 1 || chance > 10) {
-                    System.out.println("Invalid chance value (must be between 1 and 10)");
+                if (chance <= 1 || chance >= 10) {
+                    System.out.println("Invalid chance value (must be between 2 and 9)");
                     return;
                 }
 
@@ -53,7 +52,7 @@ public class Nomor3 {
                     System.out.println(current.name + "|Try Again|" + current.chance);
                     queue.add(current);
                 } else {
-                    System.out.println(current.name + "|Get Out|0");
+                    System.out.println(current.name + "|Get Out|" + current.chance);
                 }
             }
         }
